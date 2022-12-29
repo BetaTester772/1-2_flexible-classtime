d1030cc89fad048fa6b733dd106c722110e516f8
BetaTester772 <74451807+BetaTester772@users.noreply.github.com>
2022-12-29 13:54:29 +0900

    Update Orbitals.py

--- Orbitals.py
+++ Orbitals.py
@@ -8,70 +8,70 @@ Z = 1
 a = a0
 
 
-# Wave function
-def R_1_0_0(r):
+# Wave function(n, l, ml)
+def R_1_0_0(r):  # 1s
     return 2 * (Z / a) ** (3 / 2) * np.exp(-Z * r / a)
 
 
-def R_2_0_0(r):
+def R_2_0_0(r):  # 2s
     return 1 / (2 * (Z / a) ** (3 / 2)) * (2 - Z * r / a) * np.exp(-Z * r / (2 * a))
 
 
-def R_2_1_0(r):
+def R_2_1_0(r):  # 2p
     return 1 / 3 ** 1 / 2 * (2 * a) ** (3 / 2) * r / a * np.exp(-Z * r / (2 * a))
 
 
-def R_3_0_0(r):
+def R_3_0_0(r):  # 3s
     return 1 / (2 * (Z / a) ** (3 / 2)) * (18 - 9 * Z * r / a + Z ** 2 * r ** 2 / a ** 2) * np.exp(-Z * r / (3 * a))
 
 
-def R_3_1_0(r):
+def R_3_1_0(r):  # 3p
     return 1 / 3 ** 1 / 2 * (2 * a) ** (3 / 2) * r / a * (6 - Z * r / a) * np.exp(-Z * r / (3 * a))
 
 
-def R_3_2_0(r):
+def R_3_2_0(r):  # 3d
     return 1 / 3 ** 1 / 2 * (2 * a) ** (3 / 2) * r ** 2 / a ** 2 * np.exp(-Z * r / (3 * a))
 
 
-def R_4_0_0(r):
+def R_4_0_0(r):  # 4s
     return 1 / (2 * (Z / a) ** (3 / 2)) * (
             32 - 24 * Z * r / a + 6 * Z ** 2 * r ** 2 / a ** 2 - Z ** 3 * r ** 3 / a ** 3) * np.exp(
             -Z * r / (4 * a))
 
 
-def R_4_1_0(r):
+def R_4_1_0(r):  # 4p
     return 1 / 3 ** 1 / 2 * (2 * a) ** (3 / 2) * r / a * (12 - 6 * Z * r / a + Z ** 2 * r ** 2 / a ** 2) * np.exp(
             -Z * r / (4 * a))
 
 
-def R_4_2_0(r):
+def R_4_2_0(r):  # 4d
     return 1 / 3 ** 1 / 2 * (2 * a) ** (3 / 2) * r ** 2 / a ** 2 * (4 - Z * r / a) * np.exp(-Z * r / (4 * a))
 
 
-def R_4_3_0(r):
+def R_4_3_0(r):  # 4f
     return 1 / 3 ** 1 / 2 * (2 * a) ** (3 / 2) * r ** 3 / a ** 3 * np.exp(-Z * r / (4 * a))
 
 
-def R_5_0_0(r):
+def R_5_0_0(r):  # 5s
     return 1 / (2 * (Z / a) ** (3 / 2)) * (
             50 - 40 * Z * r / a + 12 * Z ** 2 * r ** 2 / a ** 2 - 2 * Z ** 3 * r ** 3 / a ** 3) * np.exp(
             -Z * r / (5 * a))
 
 
-def R_5_1_0(r):
+def R_5_1_0(r):  # 5p
     return 1 / 3 ** 1 / 2 * (2 * a) ** (3 / 2) * r / a * (20 - 12 * Z * r / a + 2 * Z ** 2 * r ** 2 / a ** 2) * np.exp(
             -Z * r / (5 * a))
 
 
-def R_5_2_0(r):
+def R_5_2_0(r):  # 5d
     return 1 / 3 ** 1 / 2 * (2 * a) ** (3 / 2) * r ** 2 / a ** 2 * (6 - 2 * Z * r / a) * np.exp(-Z * r / (5 * a))
 
 
-def R_5_3_0(r):
+def R_5_3_0(r):  # 5f
     return 1 / 3 ** 1 / 2 * (2 * a) ** (3 / 2) * r ** 3 / a ** 3 * (2 - Z * r / a) * np.exp(-Z * r / (5 * a))
 
 
-def R_5_4_0(r):
+def R_5_4_0(r):  # 5g
     return 1 / 3 ** 1 / 2 * (2 * a) ** (3 / 2) * r ** 4 / a ** 4 * np.exp(-Z * r / (5 * a))
 
 
@@ -81,7 +81,7 @@ def plotEach():  # 오비탈 각각 그리기
     functions = [R_1_0_0, R_2_0_0, R_2_1_0, R_3_0_0, R_3_1_0, R_3_2_0, R_4_0_0, R_4_1_0, R_4_2_0, R_4_3_0, R_5_0_0,
                  R_5_1_0, R_5_2_0, R_5_3_0, R_5_4_0]  # 함수들
     print(len(functions))  # 함수 개수
-    titles = ['1s', '2s', '2p', '3s', '3p', '3d', '3d', '3d', '4s', '4p', '4d', '4f', '5s', '5p', '5d', '5f',
+    titles = ['1s', '2s', '2p', '3s', '3p', '3d', '4s', '4p', '4d', '4f', '5s', '5p', '5d', '5f',
               '5g']  # 오비탈 종류
     for f in functions:  # 함수 개수만큼 반복
         plt.plot(r, f(r))  # 그래프 그리기
@@ -96,7 +96,7 @@ def plotAll():  # 오비탈 한번에 그리기
     functions = [R_1_0_0, R_2_0_0, R_2_1_0, R_3_0_0, R_3_1_0, R_3_2_0, R_4_0_0, R_4_1_0, R_4_2_0, R_4_3_0, R_5_0_0,
                  R_5_1_0, R_5_2_0, R_5_3_0, R_5_4_0]  # 함수들
     print(len(functions))  # 함수 개수
-    titles = ['1s', '2s', '2p', '3s', '3p', '3d', '3d', '3d', '4s', '4p', '4d', '4f', '5s', '5p', '5d', '5f',
+    titles = ['1s', '2s', '2p', '3s', '3p', '3d', '4s', '4p', '4d', '4f', '5s', '5p', '5d', '5f',
               '5g']  # 오비탈 종류
     for f in functions:  # 함수 개수만큼 반복
         plt.subplot(3, 5, functions.index(f) + 1)  # 그래프 위치
